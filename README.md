dba61fc439062766524e2cd897f28fd5a9d4aea3
Galen Church <galenechurch@gmail.com>
2021-11-03 17:29:21 -0400

    running

---
+++ README.md
@@ -0,0 +1,7 @@
+# POC Multi-threaded mainloop
+
+runs using rand conditions
+
+## cross-compile
+
+    cargo build --target=armv7-unknown-linux-gnueabihf
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,136 @@
-use tokio::sync::{oneshot, mpsc};
-use tokio::time::{timeout, Duration};
 use std::{thread, time};
+use std::sync::mpsc;
 
+use rand::thread_rng;
+use rand::Rng;
 
-async fn worker(chnl: oneshot::Sender<String>) {
-    let ten_millis = time::Duration::from_millis(2000);
-
-    thread::sleep(ten_millis);
-    chnl.send("workert".to_string());
-    
+#[derive(Debug, PartialEq)]
+enum MachineStates{
+    Destitute,
+    Working,
+    Idle,
 }
 
-#[tokio::main]
-async fn main() {
 
+#[derive(Debug)]
+pub struct Machine {
+    state: MachineStates,
+    pub cond_a: bool,
+    pub cond_b: bool,
+}
 
-    loop{
+impl Machine{
 
-        let (tx, mut rx) = mpsc::channel(32);
-        let tx2 = tx.clone();
-
-        tokio::spawn(async move {
-            tx.send("sending from first handle").await;
-        });
-    
-        tokio::spawn(async move {
-            tx2.send("sending from second handle").await;
-        });
-
-        while let Some(message) = rx.recv().await {
-            println!("GOT = {}", message);
-    
-        
-        }
-    
-        let (worker_tx, worker_rx) = oneshot::channel();
-        tokio::spawn(async move {
-            worker(worker_tx).await;
-        });
-       
-    
-        // Wrap the future with a `Timeout` set to expire in 10 milliseconds.
-        if let Err(_) = timeout(Duration::from_millis(1000), worker_rx).await {
-            println!("did not receive value within 10 ms");
+    pub fn new() -> Machine{
+        Machine{
+            state: MachineStates::Idle,
+            cond_a: false,
+            cond_b: false,
         }
     }
 
+    fn get_next_state(&self) -> MachineStates{
+        match self.state {
+            MachineStates::Destitute =>{
+                if self.cond_a && self.cond_b{
+                    return MachineStates::Working;
+                }
+                return MachineStates::Destitute;
+            }
+            MachineStates::Working => {
+                return MachineStates::Idle;
+            },
+            MachineStates::Idle => {
+                return MachineStates::Destitute;
+            },
+        };
+    }
+
+    pub fn machine(&mut self){
+        let next_state = self.get_next_state();
+
+        if self.state != next_state{
+            println!("New state = {:?} ", next_state);
+        }
+
+        self.state = next_state;
+        match self.state {
+            MachineStates::Destitute =>{
+                println!("DESTITUDE and waiting for conditions");
+            },
+            MachineStates::Working =>{
+                self.cond_a = false;
+                self.cond_b = false;
+
+                let dur = time::Duration::from_millis(2000);
+                thread::sleep(dur);
+                println!("WORKING Completed Sleep");
+            }
+            MachineStates::Idle =>{
+                println!("IDLE next is DESTITIUE");
+
+            }
+        }
+    }
+}
+
+fn condition_a(chan: mpsc::Sender<String>) {
+    let mut rng = thread_rng();
+
+    loop{ 
+        let sec: f32 = rng.gen_range(0.5, 10.0);
+        let dur = time::Duration::from_secs_f32(sec);
+        println!("Condition A sleep={:?}",dur);
+        thread::sleep(dur);
+        match chan.send(format!("A")){
+            Ok(_) => (),
+            Err(e) => println!("Error With Condition A send {:?}", e),
+        };
+    }
+}
+
+fn condition_b(chan: mpsc::Sender<String>) {
+    let mut rng = thread_rng();
+
+    loop{
+        let sec: f32 = rng.gen_range(0.5, 10.0);
+        let dur = time::Duration::from_secs_f32(sec);
+        println!("Condition B sleep={:?}",dur);
+        thread::sleep(dur);
+        match chan.send(format!("B")){
+            Ok(_) => (),
+            Err(e) => println!("Error With Condition B send {:?}", e),
+        };
+    }
+}
+
+fn main() {
+
+    let mut machine = Box::new(Machine::new());
+
+    let (tx, mut rx) = mpsc::channel::<String>();
+    let tx2 = tx.clone();
+
+    thread::spawn(|| {
+        condition_a(tx);
+    });
+
+    thread::spawn(move || {
+        condition_b(tx2);
+    });
+
+    loop{
+
+        while let Ok(message) = rx.recv() {
+            println!("Recieved Condition = {}", message);
+            match message.as_str(){
+                "B" => machine.cond_a = true,
+                "A" => machine.cond_b = true,
+                _ => println!("idk where that came from...")
+            };
+            
+            //Call State Machine with new condition
+            machine.machine();
+        }
+    }
 }
